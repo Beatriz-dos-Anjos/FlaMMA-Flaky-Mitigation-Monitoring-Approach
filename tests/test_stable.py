efcdc7ee45ad749954de6f7e37c2b7fee49ceb19
Beatriz-dos-Anjos <biaanjos2005@gmail.com>
2025-12-07 19:20:35 -0300

    Remove virtualenv from repo and add .gitignore

--- tests/test_stable.py
+++
@@ -1,8 +0,0 @@
-def test_upper():
-    assert "hello".upper() == "HELLO"
-
-def test_split():
-    assert "a,b,c".split(",") == ["a", "b", "c"]
-
-def test_join():
-    assert "-".join(["a", "b", "c"]) == "a-b-c"
