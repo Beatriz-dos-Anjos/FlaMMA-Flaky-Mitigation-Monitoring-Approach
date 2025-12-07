d296159c1642bf605b880487975a875d7fa968b4
Beatriz-dos-Anjos <biaanjos2005@gmail.com>
2025-12-07 16:52:23 -0300

    chore(ci): flakiness tio be tested

--- tests/test_flaky.py
+++ tests/test_flaky.py
@@ -1,9 +1,11 @@
+import random
+
 state = {"initialized": False}
 
 def test_initialize_state():
-    state["initialized"] = True
+    if random.random() < 0.5:
+        state["initialized"] = True
     assert state["initialized"]
 
 def test_use_state():
-    # flakiness ocorre quando esse executa ANTES do de cima
     assert state["initialized"]
